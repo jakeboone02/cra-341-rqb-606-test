53a95501f22f15121d4a73f2c74bdb7fde823f72
Jake Boone <jakeboone02@gmail.com>
2023-03-08 09:57:47 -0700

    Minimal reproduction of CRA 3.4.1 (webpack 4) problem with RQB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { QueryBuilder } from "react-querybuilder";
+import "react-querybuilder/dist/query-builder.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <QueryBuilder />;
 }
 
 export default App;
